b3b83547d4ec70d36930cb51feb8bbe81831e45f
Reina <reina.khasanova@iaau.edu.kg>
2021-05-31 11:29:43 +0600

    Initial commit

---
+++ src/main/java/com/example/ordering/controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.example.ordering.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String greeting(Model model) {
+        model.addAttribute("title", "главная страница");
+        return "homepage";
+    }
+}
